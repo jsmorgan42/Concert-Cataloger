b72774b08c78dab6756e45994b225fff39085b11
Jesse Morgan <jesse.morgan@willowtreeapps.com>
2022-05-17 13:51:37 -0400

    Initial Commit

---
+++ Concert Cataloger/Concert_CatalogerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concert_CatalogerApp.swift
+//  Concert Cataloger
+//
+//  Created by Jesse Morgan on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Concert_CatalogerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
